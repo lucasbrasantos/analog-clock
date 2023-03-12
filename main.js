4c76e5fbfa0dafd1a5546f0e5231edea1fdf8886
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-03-12 15:48:57 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+
+let hr = document.querySelector('.hour').style
+let min = document.querySelector('.minutes').style
+let sec = document.querySelector('.seconds').style
+
+
+setInterval(() =>{
+    
+    const time = new Date().toLocaleString("pt-BR", {timeZone: "America/Sao_Paulo"})
+    const ss = time.slice(-2);
+    const mm = time.slice(15, 17);
+    const hh = time.slice(12, 14);
+
+    let ssDeg = parseInt(ss) * 6;
+    let mmDeg = parseInt(mm) * 6;    
+    mmDeg = 90;
+    // let hhDeg = parseInt(hh) * 30 + (30/100)*(mmDeg/3.6);
+    let hhDeg = parseInt(hh) * 30 + (30*mmDeg)/360;
+
+    
+
+    
+    
+    
+
+
+    sec.rotate = `${ssDeg}deg`;
+    min.rotate = `${mmDeg}deg`;
+    hr.rotate = `${hhDeg}deg`;
+    
+    
+    
+})
