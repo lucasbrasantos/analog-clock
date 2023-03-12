39ec4d361925913d9be1b084a2b1705ab9c42c1e
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-03-12 20:20:43 -0300

    update main.js

--- main.js
+++ main.js
@@ -6,16 +6,24 @@ let sec = document.querySelector('.seconds').style
 
 setInterval(() =>{
     
+    /*
     const time = new Date().toLocaleString("pt-BR", {timeZone: "America/Sao_Paulo"})
     const ss = time.slice(-2);
     const mm = time.slice(15, 17);
     const hh = time.slice(12, 14);
+    */
+
+    const time = new Date()
+    const ss = time.getSeconds();
+    const mm = time.getMinutes();
+    const hh = time.getHours();
 
     let ssDeg = parseInt(ss) * 6;
     let mmDeg = parseInt(mm) * 6;    
-    mmDeg = 90;
-    // let hhDeg = parseInt(hh) * 30 + (30/100)*(mmDeg/3.6);
     let hhDeg = parseInt(hh) * 30 + (30*mmDeg)/360;
+    
+    
+    // let hhDeg = parseInt(hh) * 30 + (30/100)*(mmDeg/3.6);
 
     sec.rotate = `${ssDeg}deg`;
     min.rotate = `${mmDeg}deg`;
